5fd943b2df232942939cac35452405fa155c1d00
volpifc-sudo <volpi.fc@gmail.com>
2026-05-30 19:10:38 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker - Drill Control v3.3.3
-const CACHE_NAME = 'drillcontrol-cache-v3.3.3';
+// Service Worker - Drill Control v3.3.4
+const CACHE_NAME = 'drillcontrol-cache-v3.3.4';
 const ASSETS = [
     './',
     './index.html',
@@ -19,7 +19,7 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
-                console.log('[SW] Drill Control v3.2.2 - archivos almacenados en caché');
+                console.log('[SW] Drill Control v3.3.4 - archivos almacenados en caché');
                 return cache.addAll(ASSETS);
             })
             .then(() => self.skipWaiting())
